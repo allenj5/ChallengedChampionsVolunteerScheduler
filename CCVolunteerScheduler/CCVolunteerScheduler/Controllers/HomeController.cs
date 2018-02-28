03b5014333c67c053505b6b0d7950db79b79fb0f
Jonathan Allen <allenj5@findlay.edu>
2018-02-28 18:53:20 -0500

    have volunteers table created

--- CCVolunteerScheduler/CCVolunteerScheduler/Controllers/HomeController.cs
+++ CCVolunteerScheduler/CCVolunteerScheduler/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CCVolunteerScheduler.Models;
 
 namespace CCVolunteerScheduler.Controllers
 {
@@ -97,6 +98,8 @@ namespace CCVolunteerScheduler.Controllers
 
         public ActionResult Volunteers()
         {
+            VolunteersDBEntities _db = new VolunteersDBEntities();
+            ViewData.Model = _db.Volunteers.ToList();
             return View();
         }
 
--- CCVolunteerScheduler/CCVolunteerScheduler/Models/Volunteers.Context.cs
+++ CCVolunteerScheduler/CCVolunteerScheduler/Models/Volunteers.Context.cs
@@ -13,10 +13,10 @@ namespace CCVolunteerScheduler.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class VolunteersDBEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public VolunteersDBEntities()
+            : base("name=VolunteersDBEntities")
         {
         }
     
