d96f8cacb2d71ec2a600298e9a6b17bc3175a0b1
testens <testens@findlay.edu>
2018-03-12 19:39:56 -0400

    added HashingSaltModel to be able to hash and salt passwords

--- CCVolunteerScheduler/CCVolunteerScheduler/Controllers/LoginController.cs
+++ CCVolunteerScheduler/CCVolunteerScheduler/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CCVolunteerScheduler.Models;
 using System.Web.Security;
+using System.Security.Cryptography; 
 
 namespace CCVolunteerScheduler.Controllers
 {
@@ -25,7 +26,12 @@ namespace CCVolunteerScheduler.Controllers
         public ActionResult Login(User user)
         {
             VolunteersDBEntities _db = new VolunteersDBEntities();
-            string userId = _db.ValidateUser(user.Username, user.Password).FirstOrDefault();
+
+            //ComputeHash code here to check with the database
+            var salt = new Byte[16];
+            string hashedPassword = HashingSaltModel.ComputeHash(user.Password, new SHA256CryptoServiceProvider(), salt);
+
+            string userId = _db.ValidateUser(user.Username, hashedPassword).FirstOrDefault();
 
             string message = string.Empty;
 
