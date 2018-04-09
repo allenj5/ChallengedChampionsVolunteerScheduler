fcf746287c91e7c19992c8ca28aad435e5501ae4
Aziz <aldaghiria@findlay.edu>
2018-04-09 12:59:17 -0400

    Fixed styling for some  of the pages.

--- CCVolunteerScheduler/CCVolunteerScheduler/Models/AccountViewModels.cs
+++ CCVolunteerScheduler/CCVolunteerScheduler/Models/AccountViewModels.cs
@@ -110,7 +110,7 @@ namespace CCVolunteerScheduler.Models
     public class ChangePasswordViewModel
     {
         [Required]
-        [Display(Name = "Old Password")]
+        [Display(Name = "Current Password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
