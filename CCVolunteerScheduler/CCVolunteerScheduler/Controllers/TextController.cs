a6e76c2a91176f963a41411425db72380be9fb11
Testens <Testens@DESKTOP-P6MCV7F>
2018-04-26 20:31:29 -0400

    Texting all volunteers today working fam

--- CCVolunteerScheduler/CCVolunteerScheduler/Controllers/TextController.cs
+++ CCVolunteerScheduler/CCVolunteerScheduler/Controllers/TextController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CCVolunteerScheduler.Models;
 using Twilio;
 using Twilio.Types;
 using Twilio.Rest.Api.V2010.Account;
@@ -30,7 +31,32 @@ namespace CCVolunteerScheduler.Controllers
                     );
             }
 
-            return View("~/Views/TextVolunteers.cshtml");
+            return RedirectToAction("Communications", "Home");
+        }
+
+
+        public ActionResult TextTodaysVolunteers()
+        {
+            DateTime today = DateTime.Now;
+
+            VolunteersDBEntities _db = new VolunteersDBEntities();
+
+            var accountsid = "AC1e234cf4a6742315d15d6d0f457c3d55";
+            var authToken = "8f0c777ebd152c1145b6c90f82d29ac4";
+            TwilioClient.Init(accountsid, authToken);
+
+            var from = new PhoneNumber("+14193703444");
+
+            foreach (string email in _db.TodaysVolunteersPhone(today.Date))
+            {
+                var message = MessageResource.Create(
+                   to: "+1" + email,
+                   from: from,
+                   body: "Just making sure it goes back to the Communications page after they press the button"
+                   );
+            }
+
+            return RedirectToAction("Communications", "Home");
         }
 
     }
